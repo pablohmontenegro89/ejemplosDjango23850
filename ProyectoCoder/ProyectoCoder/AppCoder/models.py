e23ff7c9104ee78b6f13ef7578394f0012f3aca2
pablohmontenegro89 <pablohmontenegro8@gmail.com>
2021-12-02 22:30:11 -0300

    fin de la clase 19

--- ProyectoCoder/ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/models.py
@@ -7,3 +7,10 @@ class Curso(models.Model):
 
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
+
+
+class Jugador(models.Model):
+
+    apellido = models.CharField(max_length=40)
+    numero = models.IntegerField()
+    esBueno = models.BooleanField()
--- ProyectoCoder/ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# Primer vista
+def inicio(request):
+    # return HttpResponse("Esto es una prueba de inicio")
+    return render(request, 'AppCoder/inicio.html')
+
+
+def jugadores(request):
+    return render(request, 'AppCoder/jugadores.html')
